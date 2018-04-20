c5b6119332e76496a8f592d71a4982f5e3d77a9d
cb <csbutton01@hotmail.com>
2018-04-20 15:26:39 -0700

    now calculate age on Venus and Mars

--- src/birthday.js
+++ src/birthday.js
@@ -4,6 +4,7 @@ class Birthday {
     this.minutes = 1;
     this.minutesInHour = 60;
     this.hoursInDay = 24;
+    this.lifeExpectancy = 80;
   }
 
   secondsToMinutes() {
@@ -24,9 +25,24 @@ class Birthday {
   mercuryAge(year, month, day){
     let onEarth = new Date(year, month-1, day);
     let mercuryReletivity = .28;
-    let myAgeOnMercury = (Date.now() - onEarth)/1000 * .28;
+    let myAgeOnMercury = (Date.now() - onEarth)/1000 * mercuryReletivity;
     return myAgeOnMercury;
   }
+
+  venusAge(year, month, day){
+    let onEarth = new Date(year, month-1, day);
+    let venusReletivity = .62;
+    let myAgeOnVenus = (Date.now() - onEarth)/1000 * venusReletivity;
+    return myAgeOnVenus;
+  }
+
+  marsAge(year, month, day){
+    let onEarth = new Date(year, month-1, day);
+    let marsReletivity = 1.88;
+    let myAgeOnMars = (Date.now() - onEarth)/1000 * marsReletivity;
+  }
+
+
 
 }
 
